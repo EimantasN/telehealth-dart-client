4ac1e36c081f5f4b790eb90b7ee1775f71ecd1aa
Eimantas Noreika <noreika.eimantas@gmail.com>
2022-08-21 14:23:14 +0000

    Release for commit: Add Functionality To create folder

--- lib/model/create_folder_cmd.dart
+++ lib/model/create_folder_cmd.dart
@@ -17,12 +17,6 @@ class CreateFolderCmd {
     this.folderName,
   });
 
-  ///
-  /// Please note: This property should have been non-nullable! Since the specification file
-  /// does not include a default value (using the "default:" property), however, the generated
-  /// source code must fall back to having a nullable type.
-  /// Consider adding a "default:" property in the specification file to hide this note.
-  ///
   int? parentId;
 
   ///
