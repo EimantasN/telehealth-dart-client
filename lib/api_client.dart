92551aa22c72ba8574778239bb4d819a75d005cd
Eimantas Noreika <noreika.eimantas@gmail.com>
2023-02-19 17:25:50 +0000

    [master] Release for commit: Remove console logs.

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -11,7 +11,7 @@
 part of openapi.api;
 
 class ApiClient {
-  ApiClient({this.basePath = 'https://api.telehealth.endev.lt', this.authentication,});
+  ApiClient({this.basePath = 'https://api.viallies.com', this.authentication,});
 
   final String basePath;
   final Authentication? authentication;
